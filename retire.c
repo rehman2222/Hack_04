545faedb8345835ac187fae4a319b275e07eb3aa
Rehman <rk03229975701@gmail.com>
2023-10-22 07:24:58 -0700

    declear variable according to the program

---
+++ retire.c
@@ -0,0 +1,12 @@
+//The prgram about 401(k) plan is a account for people to save money for retirement
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main(){
+
+    float intilaAmount, mothlycontributionAmount, annualReturn, annualInflation, balance;
+    float  interest, ajdustinflationRetrunRate, totalInterestEarned = 0;
+    int retirement, month; 
+}
